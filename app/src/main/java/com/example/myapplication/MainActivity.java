a997f74bb4579376fe9a55e77c2aee8de4c963d0
Saad Ali Khan <saad.niaxi47@gmail.com>
2021-11-18 00:47:00 +0500

    Dialogbox implemented with intent

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 
 import android.os.Bundle;
@@ -14,12 +16,35 @@ import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity  {
-    Button b;
+    Button b,e;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         b=findViewById(R.id.button1);
+        e=findViewById(R.id.exit);
+        e.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Alert!");
+                builder.setMessage("Do You Want to Exit?");
+                builder.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(getApplicationContext(),adan.class);
+                        startActivity(intent);
+                    }
+                });
+                builder.setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                builder.show();
+            }
+        });
         b.setOnClickListener(v -> {
             LayoutInflater li = getLayoutInflater();
             View layout= li.inflate(R.layout.my_layout,(ViewGroup) findViewById(R.id.custom));
